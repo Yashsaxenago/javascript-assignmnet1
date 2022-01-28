b9401f73e178060d0f2413a5a55fdc3cda60a8dd
Yash Saxena <95130859+Yashsaxenago@users.noreply.github.com>
2022-01-28 13:19:04 +0530

    Add files via upload

---
+++ posi neg zero.js
@@ -0,0 +1,10 @@
+const number =67;
+if (number >= 0) {
+    if (number == 0) {
+        console.log("zero");
+    } else {
+        console.log("positive");
+    }
+} else {
+    console.log("negative");
+}
